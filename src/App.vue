c0d89c62804843ec6b99abbe4861d986e490dec6
Bartosz Wrobel <bartoszwrobel99@gmail.com>
2021-01-16 17:04:45 +0100

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,137 @@
 <template>
   <v-app class="app">
     <v-app-bar app color="#121212" dark flat>
-      <v-toolbar-title>IPDS</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>
+        <span style="font-weight: bold"> IPDS </span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
     </v-app-bar>
 
     <v-main>
       <v-container fill-height fluid>
         <v-row>
-          <v-col cols="4">
-            <v-navigation-drawer permanent>
+          <v-col cols="3">
+            <v-navigation-drawer
+              permanent
+              class="d-flex flex-row align-center justify-center"
+            >
               <v-list-item>
-                <v-list-item-content>
-                  <v-list-item-title>Menu?</v-list-item-title>
+                <v-list-item-content
+                  ><span style="font-weight: bold"> Hi!</span> <br />
+                  Click on the map to add the delivery points.
                 </v-list-item-content>
               </v-list-item>
+              <v-form>
+                <v-list-item class="mt-10">
+                  <v-list-item-content>
+                    Use this slider to adjust starting petrol level in litres.
+                    <v-slider
+                      class="mt-6 px-6"
+                      min="1"
+                      max="10"
+                      thumb-label
+                      v-model="petrol"
+                    ></v-slider>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-container
+                  class="d-flex flex-column align-center justify-center"
+                >
+                  <v-btn
+                    outlined
+                    @click="send"
+                    :disabled="places.length < 3"
+                    :loading="loading"
+                  >
+                    CALCULATE ROUTE!
+                  </v-btn>
+                </v-container>
+              </v-form>
             </v-navigation-drawer>
           </v-col>
-          <v-col cols="8">
-            <g-map />
+          <v-col cols="5">
+            <g-map
+              :solution="solution"
+              :markerToRemove="marker"
+              @places-changed="getPlaces"
+            />
+          </v-col>
+          <v-col cols="4" class="pr-6">
+            <v-row
+              style="font-weight: bold"
+              class="d-flex flex-row align-center pt-2 pb-5 pr-3"
+            >
+              <v-col> PLACES </v-col>
+              <v-btn
+                color="error"
+                outlined
+                @click="removeMarker(places[places.length - 1])"
+                v-if="places.length > 0"
+              >
+                REMOVE LAST
+              </v-btn>
+            </v-row>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Index</th>
+                    <th class="text-left">Coordinates</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr v-for="place in places" :key="places.indexOf(place)">
+                    <td>{{ places.indexOf(place) + 1 }}</td>
+                    <td>{{ place }}</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+            <v-row
+              v-if="Object.keys(solution).length > 0"
+              class="d-flex flex-row align-center mt-10"
+            >
+              <v-col cols="12">
+                <span style="font-weight: bold"> SOLUTION </span></v-col
+              >
+              <v-col cols="12">
+                <v-simple-table>
+                  <template v-slot:default>
+                    <thead>
+                      <tr>
+                        <th class="text-left">Name</th>
+                        <th class="text-left">Value</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td>Order</td>
+                        <td>{{ showOrder() }}</td>
+                      </tr>
+                      <tr>
+                        <td>Delivery time</td>
+                        <td>{{ solution.delivery_time }} minutes</td>
+                      </tr>
+                      <tr>
+                        <td>Distance covered</td>
+                        <td>{{ solution.km }} km</td>
+                      </tr>
+                      <tr>
+                        <td>Pizza temperature</td>
+                        <td>{{ solution.pizza_temperature }} °C</td>
+                      </tr>
+                      <tr>
+                        <td>Fuel used [litres]</td>
+                        <td>{{ solution.used_fuel }} litres</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-container>
@@ -27,7 +141,7 @@
 
 <script>
 import GMap from "./views/GMap";
-
+import axios from "axios";
 export default {
   name: "App",
 
@@ -36,15 +150,60 @@ export default {
   },
 
   data: () => ({
-    //
+    places: [],
+    marker: [],
+    petrol: 1,
+    solution: {},
+    loading: false,
   }),
 
-  created() {},
+  methods: {
+    getPlaces(places) {
+      this.places = places;
+    },
+    removeMarker(marker) {
+      this.marker = marker;
+    },
+    showOrder() {
+      let strOrder = "1 ";
+      for (let idx of this.solution.order) {
+        if (idx > 9) {
+          strOrder += "ST ";
+        } else {
+          strOrder += (idx + 1).toString() + " ";
+        }
+      }
+      strOrder += "1";
+      return strOrder;
+    },
+    send() {
+      return new Promise((resolve, reject) => {
+        this.loading = true;
+        axios
+          .post("http://localhost:5000/calculate", {
+            places: this.places,
+            petrol: this.petrol,
+          })
+          .then((res) => {
+            this.loading = false;
+            this.solution = res.data;
+            resolve(true);
+          })
+          .catch((err) => {
+            console.log(err);
+            reject();
+          });
+      });
+    },
+  },
 };
 </script>
 
 <style>
-.app {
-  font-family: "Montserrat";
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap");
+
+#app {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
 }
 </style>
--- src/views/GMap.vue
+++ src/views/GMap.vue
@@ -9,11 +9,18 @@ import GoogleMapsApiLoader from "google-maps-api-loader";
 
 export default {
   name: "GMap",
+  props: {
+    markerToRemove: Array,
+    solution: {},
+  },
   data() {
     return {
       google: null,
       map: null,
       mapCenter: { lat: 50.0622965721804, lng: 19.9374022326302 },
+      places: [],
+      markers: [],
+      num: 0,
     };
   },
   async mounted() {
@@ -23,6 +30,23 @@ export default {
     this.google = googleMapApi;
     this.initializeMap();
   },
+  watch: {
+    places(val) {
+      this.$emit("places-changed", val);
+    },
+    markerToRemove(val) {
+      let marker = this.markers.find((m) => {
+        return m.position.lat() == val[0] && m.position.lng() == val[1];
+      });
+      marker.setMap(null);
+      let idx = this.places.indexOf(val);
+      this.places.splice(idx, 1);
+      this.num = idx;
+    },
+    solution(val) {
+      this.draw(val.order, val.petrol_locations);
+    },
+  },
   methods: {
     initializeMap() {
       const mapContainer = this.$refs.googleMap;
@@ -30,6 +54,66 @@ export default {
         center: this.mapCenter,
         zoom: 15,
       });
+      this.map.addListener("click", (mapsMouseEvent) => {
+        this.places.push([
+          mapsMouseEvent.latLng.toJSON()["lat"],
+          mapsMouseEvent.latLng.toJSON()["lng"],
+        ]);
+        let map = this.map;
+        this.markers.push(
+          new this.google.maps.Marker({
+            position: mapsMouseEvent.latLng,
+            map,
+            label: this.places.length.toString(),
+            title: "Marker",
+          })
+        );
+      });
+    },
+    draw(order, petrol) {
+      let sol = [];
+      let map = this.map;
+      for (let idx of order) {
+        if (idx > 9) {
+          sol.push({
+            lat: petrol[idx - 10][0],
+            lng: petrol[idx - 10][1],
+          });
+          this.markers.push(
+            new this.google.maps.Marker({
+              position: {
+                lat: petrol[idx - 10][0],
+                lng: petrol[idx - 10][1],
+              },
+              map,
+              label: "ST",
+              title: "Marker",
+            })
+          );
+        } else {
+          sol.push({
+            lat: this.places[idx][0],
+            lng: this.places[idx][1],
+          });
+        }
+      }
+      sol.unshift({
+        lat: this.places[0][0],
+        lng: this.places[0][1],
+      });
+      sol.push({
+        lat: this.places[0][0],
+        lng: this.places[0][1],
+      });
+      const flightPlanCoordinates = sol;
+      const flightPath = new this.google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+      });
+      flightPath.setMap(this.map);
     },
   },
 };
